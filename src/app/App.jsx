4c4596908b62b05913ace19335553e231b6a49fb
Subham777-max <subhamray865@gmail.com>
2026-08-24 23:47:48 +0530

    feat: enhance routing and authentication setup in App component

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,22 +1,29 @@
 import useTenant from "@/global/hooks/useTenant";
 import Loader from "@/global/loader/Loader"
 import { useEffect } from "react";
+import { RouterProvider } from "react-router-dom";
+import { router } from "./router/Router";
+import useAuth from "@/features/auth/hooks/useAuth";
 
 function App() {
   const { fetchTenant , loading: tenantLoading ,initialized } = useTenant();
+  const { getCurrentUser , loading: authLoading } = useAuth();
   useEffect(()=>{
     console.log("running...");
     fetchTenant();
-  },[fetchTenant])
-  if(tenantLoading){
-    return <Loader visible={tenantLoading} />
+    getCurrentUser();
+  },[fetchTenant, getCurrentUser])
+
+  if(tenantLoading || authLoading){
+    return <Loader visible={tenantLoading || authLoading} />
   }
   if(!tenantLoading && !initialized){
     return <div className="flex items-center justify-center h-screen text-red-500">This is not a valid tenant</div>
   }
+  
   return (
     <>
-      
+      <RouterProvider router={router} />
     </>
   )
 }
--- src/app/router/Router.jsx
+++ src/app/router/Router.jsx
@@ -1,5 +1,21 @@
-import { createBrowserRouter } from "react-router-dom";
-
+import { createBrowserRouter, Navigate } from "react-router-dom"
+import AuthLayout from "@/features/auth/layouts/AuthLayout"
+import LoginContainer from "@/features/auth/containers/LoginContainer"
+import RegisterContainer from "@/features/auth/containers/RegisterContainer"
+import ProtectedRoutes from "@/components/security/ProtectedRoutes"
 export const router = createBrowserRouter([
-    
-]);
+  {
+    path: "/auth",
+    element: <AuthLayout />,
+    children: [
+      { index: true, element: <Navigate to="login" replace /> },
+      { path: "login",    element: <LoginContainer /> },
+      { path: "register", element: <RegisterContainer /> },
+    ],
+  },
+  { 
+    path: "/", 
+    element: <ProtectedRoutes><div>Home</div></ProtectedRoutes>
+  },
+  { path: "*", element: <Navigate to="/auth/login" replace /> },
+])
