dc2fa0b26eebdf333e93378f5573378f3b143f89
Subham777-max <subhamray865@gmail.com>
2026-08-31 09:28:43 +0530

    feat: add chatKeys and dmService for managing direct messages and chat keys

---
+++ src/features/chats/services/dm.service.js
@@ -0,0 +1,51 @@
+import api from "@/api/api";
+
+const dmService = {
+    getDms: async () => {
+        const response = await api.get("/chats/dm");
+        return response.data.data.dms;
+    },
+    sendMessage: async (chatId, recieverId, content , replyTo=null) => {
+        const response = await api.post("/chats/dm/message", {
+            chatId,
+            recieverId,
+            content,
+            replyTo,
+        });
+        return response.data.data.message;
+    },
+    getMessages: async (chatId, limit, before) => {
+        const response = await api.get("/chats/dm/messages", {
+            params: {
+                chatId,
+                limit,
+                before,
+            },
+        });
+        return response.data.data.messages;
+    },
+    updateMessage: async (messageId, content) => {
+        const response = await api.put("/chats/dm/message", {
+            messageId,
+            content,
+        });
+        return response.data.data.message;
+    },
+    deleteMessage: async (messageId) => {
+        const response = await api.delete("/chats/dm/message", {
+            data: {
+                messageId,
+            },
+        });
+        return response.data.data.message;
+    },
+    addReaction: async (messageId, emoji) => {
+        const response = await api.post("/chats/dm/message/reaction", {
+            messageId,
+            emoji,
+        });
+        return response.data.data.message;
+    },
+}
+
+export default dmService;
