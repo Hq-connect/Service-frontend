99035ef2e56f28d76c1282dfd7b39bd5012c725d
extremecoder-rgb <hsuswiowkskow@gmail.com>
2026-09-08 19:40:51 +0530

    fix(store): resolve merge conflict in root store reducer configuration

--- src/app/store/store.js
+++ src/app/store/store.js
@@ -1,20 +1,14 @@
 import { configureStore } from '@reduxjs/toolkit'
 import tenantReducer from '../../global/states/tenant.slice.js'
 import authReducer from '../../features/auth/states/auth.states.js'
-<<<<<<< Updated upstream
 import chatReducer from '../../features/chats/states/chat.slice.js'
-=======
 import meetingsReducer from '../../features/meets/states/meeting.slice.js'
 
->>>>>>> Stashed changes
 export const store = configureStore({
   reducer: {
     tenant: tenantReducer,
     auth: authReducer,
-<<<<<<< Updated upstream
     chat: chatReducer,
-=======
     meetings: meetingsReducer,
->>>>>>> Stashed changes
   },
-})
+})
